c70a77bf206795655de2b6bdabdd3d7aa3332c58
Eduardo Budny <budny.dev@gmail.com>
2021-09-09 20:08:37 -0300

    added dislike logic

--- src/disliked/disliked.entity.ts
+++ src/disliked/disliked.entity.ts
@@ -5,6 +5,9 @@ import {
   CreateDateColumn,
   OneToMany,
   PrimaryGeneratedColumn,
+  OneToOne,
+  ManyToMany,
+  ManyToOne,
 } from 'typeorm';
 // Entities
 import { UserEntity } from '../user/user.entity';
@@ -14,14 +17,12 @@ import { ProductEntity } from '../product/product.entity';
 export class DislikedEntity extends BaseEntity {
   @PrimaryGeneratedColumn('uuid') id: string;
 
-  @OneToMany(() => ProductEntity, (product) => product, {
-    cascade: true,
+  @ManyToOne(() => ProductEntity, (product) => product.id, {
     primary: true,
   })
   product: ProductEntity;
 
-  @OneToMany(() => UserEntity, (user) => user, {
-    cascade: true,
+  @ManyToOne(() => UserEntity, (user) => user.id, {
     primary: true,
   })
   user: UserEntity;
--- src/disliked/disliked.service.ts
+++ src/disliked/disliked.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProductEntity } from 'src/product/product.entity';
+import { UserEntity } from 'src/user/user.entity';
+import { Repository } from 'typeorm';
+import { DislikedEntity } from './disliked.entity';
 
 @Injectable()
-export class DislikedService {}
+export class DislikedService {
+  constructor(
+    @InjectRepository(DislikedEntity)
+    private readonly DislikedRepository: Repository<DislikedEntity>,
+  ) {}
+
+  async isDislikedByUser(userId: string, productId: string): Promise<boolean> {
+    return !!(await this.DislikedRepository.findOne({
+      relations: ['user', 'product'],
+      where: { product: { id: productId }, user: { id: userId } },
+    }));
+  }
+
+  async isDisliked(productId: string): Promise<DislikedEntity> {
+    return await this.DislikedRepository.createQueryBuilder('liked')
+      .leftJoinAndSelect('disliked.product', 'product')
+      .leftJoinAndSelect('disliked.users', 'user')
+      .where('product.id = :productId', { productId })
+      .getOne();
+  }
+
+  async addNewDislike(
+    user: UserEntity,
+    product: ProductEntity,
+  ): Promise<DislikedEntity> {
+    const newDisliked = new DislikedEntity();
+    newDisliked.user = user;
+    newDisliked.product = product;
+
+    return await this.DislikedRepository.save(newDisliked);
+  }
+}
